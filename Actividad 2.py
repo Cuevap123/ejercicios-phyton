ffa846eca2abaa19a2063966ba7067c77ed9b626
Cuevap123 <cuevaandrea298@hotmail.com>
2026-04-08 13:24:14 -0500

    Primera actividad :D

---
+++ Actividad 2.py
@@ -0,0 +1,8 @@
+#Actividad 
+primer_nombre, segundo_nombre, pais, edad, es_casado = "Andrea", "Carolina", "Ecuador", 16, False
+print(primer_nombre, segundo_nombre, pais, edad, es_casado)
+print("Primer nombre:", primer_nombre)
+print("Segundo nombre:", segundo_nombre)
+print("Pais:", pais)
+print("Edad:", edad)
+print("Casado:",es_casado)
