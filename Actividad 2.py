2dc8898245ab23651f0b1063e8aba6d50f7c80ef
Cuevap123 <cuevaandrea298@hotmail.com>
2026-04-10 08:26:46 -0500

    Cambio 1

--- Actividad 2.py
+++ Actividad 2.py
@@ -1,4 +1,4 @@
-#Actividad 
+#Actividad 8/4/2026
 primer_nombre, segundo_nombre, pais, edad, es_casado = "Andrea", "Carolina", "Ecuador", 16, False
 print(primer_nombre, segundo_nombre, pais, edad, es_casado)
 print("Primer nombre:", primer_nombre)
---
+++ variables.py
@@ -0,0 +1,5 @@
+"""Andrea Cueva 
+10/4/2026
+Variables"""
+nombre, apellido, nombre_completo, pais, ciudad, año, esta_casado, es_verdadero,luz_encendida,  = "Andrea", "Cueva", "Andrea Cueva", "Ecuador", "Quito", False, 
+
