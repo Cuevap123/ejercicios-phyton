dc49511b5348d1452a5a6a6d10015c28cf335953
Cuevap123 <cuevaandrea298@hotmail.com>
2026-04-15 13:00:04 -0500

    Cambio 12:59

--- Actividad 2 (Variables).py
+++ Actividad 2 (Variables).py
@@ -1,8 +1,3 @@
 #Actividad 8/4/2026
 primer_nombre, segundo_nombre, pais, edad, es_casado = "Andrea", "Carolina", "Ecuador", 16, False
-print(primer_nombre, segundo_nombre, pais, edad, es_casado)
-print("Primer nombre:", primer_nombre)
-print("Segundo nombre:", segundo_nombre)
-print("Pais:", pais)
-print("Edad:", edad)
-print("Casado:",es_casado)
+print(f"primer mnombre: {primer_nombre}\nsegundo nombre: {segundo_nombre}\npais: {pais}\nedad: {edad}\nes casado: {es_casado}") 
--- Actividad 4 (strings).py
+++ Actividad 4 (strings).py
@@ -1,1 +1,4 @@
-
+nombre, apellido  = "Andrea", "Cueva"
+nombre_completo = nombre+" "+apellido
+print (nombre_completo)
+print ("I think I'm too cool to know ya\n You say I'm like the ice, I freeze \n I'm churning out novels like \n Beat poetry on amphetamines")
