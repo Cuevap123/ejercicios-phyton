1fb39359b97a4f479c6228a181a2299f2457d838
Cuevap123 <cuevaandrea298@hotmail.com>
2026-04-15 13:27:16 -0500

    Cambio 13:27

--- Actividad 4 (strings).py
+++ Actividad 4 (strings).py
@@ -2,3 +2,12 @@ nombre, apellido  = "Andrea", "Cueva"
 nombre_completo = nombre+" "+apellido
 print (nombre_completo)
 print ("I think I'm too cool to know ya\n You say I'm like the ice, I freeze \n I'm churning out novels like \n Beat poetry on amphetamines")
+#Desempaquetar nombre
+nombre = "Andrea"
+a,b,c,d,e,f = nombre
+print (a,b,c,d,e,f)
+first_letter = nombre[0]
+print(first_letter) 
+print (nombre[3:6])
+print (nombre[::-1])
+
