67b5214be3eb55d063f49f4276c36d277755d9f2
Cuevap123 <cuevaandrea298@hotmail.com>
2026-04-15 12:08:27 -0500

    Modificacion

--- variables.py
+++ variables.py
@@ -38,35 +38,42 @@ edad = int(input("Edad: "))
 help('keywords')
 #DEBER
 #ejercicio 1
+
 edad = 16
 #ejercio 2
+
 estatura = 1.60
 #ejercicio 3
+
 base = float(input("Ingrese la base: "))
 altura = float(input("Ingrese la altura: "))
 area = 0.5 * base * altura 
 print ("El area de tu triangulo es: ",area)
 #ejercicio 4
+
 a = float(input("Ingrese el lado a: "))
 b = float(input("Ingrese el lado b: "))
 c = float(input("Ingrese el lado c: "))
 perimetro = a + b + c
 print ("El perimetro de tu triangulo es: ",perimetro)
 #ejercicio 5
+
 longitud = float(input("Ingrese la longitud: "))
 ancho = float(input("Ingrese el ancho: "))
 area = longitud * ancho
 perimetro = 2 * (longitud + ancho)
-print("Área:", area)
-print("Perímetro:", perimetro)
+print("El area es:", area)
+print("El perimetro es:", perimetro)
 #ejercicio 6
+
 radio = float(input("Ingrese el radio: "))
 pi = 3.14
-area = pi * radio * radio
+area = pi * radio **2 
 circunferencia = 2 * pi * radio
-print("Área:", area)
-print("Circunferencia:", circunferencia)
+print("El area es:", area)
+print("La circunferencia :", circunferencia)
 #ejercicio 7
+
 x1, y1 = 2, 2
 x2, y2 = 6, 10
 m = (y2 - y1) / (x2 - x1)
@@ -74,31 +81,52 @@ distancia = ((x2 - x1)**2 + (y2 - y1)**2) ** 0.5
 print("La pendiente es:", m)
 print("La distancia es:", distancia)
 #ejercicio 7
+
 x = int(input("Ingrese un valor de x: "))
 y = x**2 + 6*x + 9
 print("y =", y)
 x = -3
+#ejercicio 8
+
 a = len("python")
 b = len("dragon")
 print(a == b)
 print("on" in "python" and "on" in "dragon")
+#ejercicio 9
+
 oracion = "Espero que este curso no esté lleno de jerga."
 print("jerga" in oracion)
+#ejercici 10
+
 print("on" in "python" and "on" in "dragon")
+#ejercicio 11
+
 longitud = len("python")
 a = float(longitud)
 b = str(a)
 print(b)
+#ejercicio 12
+
 print(7 // 3 == int(2.7))
+#ejercicio 13
+
 print(type("10") == type(10))
+#ejercicio 14
+
 print(int(float('9.8')) == 10)
+#ejercicio 15
+
 horas = float(input("Horas trabajadas: "))
 tarifa = float(input("Tarifa por hora: "))
 pago = horas * tarifa
 print("Pago total:", pago)
+#ejercicio 16
+
 años = int(input("Años vividos: "))
 segundos = años * 365 * 24 * 60 * 60
 print("Segundos vividos:", segundos)
+#ejercicio 17
+
 for i in range(1, 6):
     print(i, 1, i, i**2, i**3)
 
