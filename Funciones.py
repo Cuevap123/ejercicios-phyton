128e219549d0a9797b64cc984b5d9a94d93a50ef
Andrea_Cueva <cuevaandrea298@gmail.com>
2026-06-03 12:24:50 -0500

    Primer

--- Funciones.py
+++ Funciones.py
@@ -1,1 +1,9 @@
-
+def obtener_mensaje(mensaje):
+    return mensaje
+def generar_nombre_completo(nombre,apellido):
+    nombre_completo = nombre + " " + apellido
+    return nombre_completo
+mensaje = str(input("Ingrese el mensaje: "))
+nombre = str(input("Ingrese el nombre: "))
+apellido = str(input("Ingrese el apellido: "))
+print(f"{obtener_mensaje(mensaje)}, {generar_nombre_completo(nombre,apellido)}")
